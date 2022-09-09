518d027e005b92b4d7e1a97b8d2501bfa270cae3
Dennis Kamau <dkamau476@gmail.com>
2022-09-09 08:00:54 -0700

    Adding 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	while (c < 10)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
