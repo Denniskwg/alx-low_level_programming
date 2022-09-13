fed1d3119b90b081d7ad3f736adb4dd24f07bec4
Dennis Kamau <dkamau476@gmail.com>
2022-09-13 06:16:50 -0700

    Adding 9-times_table.c file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints the 9 times table
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int sum;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			sum = i * j;
+			if (sum > 9)
+			{
+				_putchar(sum / 10 + '0');
+				_putchar(sum % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(sum + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
