569ee6a0702089f97f687e5816ab6f06ab59ea66
Dennis Kamau <dkamau476@gmail.com>
2022-09-15 08:13:26 -0700

    Adding 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz\t");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\t");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz\t");
+		}
+		else
+		{
+		printf("%d\t", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
