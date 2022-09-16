7ebd8de7a4e443322c3ae4604eaff93c984ea61d
Dennis Kamau <dkamau476@gmail.com>
2022-09-16 03:50:24 -0700

    Bug fixes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,21 +9,15 @@ int main(void)
 {
 	long num = 612852475143;
 	int count;
-	int numbers[4];
-	int i;
 
 	for (count = 2; num > 1; count++)
 	{
 		while (num % count == 0)
 		{
-			for (i = 0; i < 4; i++)
-			{
-				numbers[i] = count;
-			}
 			num = num / count;
 		}
 	}
-	printf("%d ", numbers[2]);
+	printf("%d", count);
 	printf("\n");
 	return (0);
 }
