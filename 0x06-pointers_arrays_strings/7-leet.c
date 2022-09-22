ad8f5f143e70c29770bc96c4c112da5999bba0c7
Dennis Kamau <dkamau476@gmail.com>
2022-09-22 08:02:04 -0700

    Corrections

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,13 +11,14 @@ char *leet(char *str)
 	int i;
 	int j;
 	char letters[] = "aeotl"
+	char Letters[] = "AEOTL"
 	char numbers[] = "43071"
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; letters[j] != '\0'; j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (str[i] == letters[j] || str[i] == letters[j] - 32)
+			if (str[i] == letters[j] || str[i] == Letters[j])
 			{
 				str[i] = numbers[j];
 				break;
