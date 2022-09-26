da6c214b3e4e0ad153595f744f2bf42e552007da
Dennis Kamau <dkamau476@gmail.com>
2022-09-26 08:19:55 -0700

    correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,9 +19,8 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			if (accept[i] == s[j])
 				counter++;
+				break;
 		}
-		else if ((accept[i + 1]) == '\0')
-			return (counter);
 	}
 	return (counter);
 }
