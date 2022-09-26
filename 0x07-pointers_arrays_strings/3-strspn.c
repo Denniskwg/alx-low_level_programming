6acfee85001974b85b5b1ccbd82fc1c5908eb9f2
Dennis Kamau <dkamau476@gmail.com>
2022-09-26 06:46:13 -0700

    Adding 3-strspn.c and modified main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check initial segment
+ * @accept: string to compare to initial segment of s
+ * Return: number of bytes in the initial segment of s which consist
+ * only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int counter = 0;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != ','; j++)
+		{
+			if (accept[i] == s[j])
+				counter++;
+		}
+	}
+	return (counter);
+}
