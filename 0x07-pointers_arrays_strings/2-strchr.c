446d4e2bbdcb3e5528441b1b72dd7f671d5bd8cb
Dennis Kamau <dkamau476@gmail.com>
2022-09-26 03:58:26 -0700

    Corrections

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-		else if (*(s + 1) == c)
-		{
-			return (s + 1);
-		}
-		i++;
-	}
-	return (NULL);
+	for (i = 0; (s[i] != c) && (s[i] != '\0'; i++)
+			;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
