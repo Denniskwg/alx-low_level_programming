3747def793a69922b435b887e8cf4ce7b0c608f9
Dennis Kamau <dkamau476@gmail.com>
2022-10-11 00:48:00 -0700

    Adding dog.h and readme file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - defines a new struct dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
