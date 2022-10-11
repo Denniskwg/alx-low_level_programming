100bff77cf48aa3bf9864bd0000fd79353c07445
Dennis Kamau <dkamau476@gmail.com>
2022-10-11 03:39:19 -0700

    Correction to 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(1);
+		return;
 	if ((*d).name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == 0)
+	if ((*d).age < 0)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %.6f\n", d->age);
