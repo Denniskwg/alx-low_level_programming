3d6bb11d0b9a1f37d48cb4f3da5e76d9ddbf25ed
Dennis Kamau <dkamau476@gmail.com>
2022-10-11 13:14:00 -0700

    Corrections to 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(x) ((x) < 0 ? (x * -1) : (x))
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
 #endif /* ABS(x) */
