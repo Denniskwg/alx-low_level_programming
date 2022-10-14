b923b3b58a12c6ae8c746c7393c4d62d8c297122
Dennis Kamau <dkamau476@gmail.com>
2022-10-14 04:09:39 -0700

    Adding 1-print_numbers and modified header file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers supplied as arguments followed by
+ * a new line
+ * @separator: string to be printed between numbers
+ * @n: arguments count
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0, j;
+
+	if (n == 0)
+		return;
+	va_start(ap, n);
+	j = n;
+	while (i < n)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i < (j - 1))
+			printf("%s", separator);
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
