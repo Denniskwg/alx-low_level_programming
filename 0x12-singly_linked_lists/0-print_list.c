0944e38574de05538513e246b41f9d286a5ce75c
Dennis Kamau <dkamau476@gmail.com>
2022-10-24 02:58:03 -0700

    Adding header file, readme and 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to a list_t
+ * Return: number of nodes of type list_t
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned long i;
+	const list_t *ptr;
+	char *s;
+	unsigned int j;
+
+	if (h == NULL)
+		return (0);
+	ptr = h;
+	s = ptr->str;
+	j = ptr->len;
+	i = 1;
+	while (ptr->next != NULL)
+	{
+		i++;
+		if (s == NULL)
+		{
+			j = 0;
+			s  = "(nil)";
+		}
+		printf("[%u]", j);
+		printf(" %s\n", s);
+		ptr = ptr->next;
+	}
+	printf("[%u]", ptr->len);
+	printf(" %s\n", ptr->str);
+	return (i);
+}
