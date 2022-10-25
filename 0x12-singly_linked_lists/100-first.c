75817254488ad2d707ac6dc7f8e7ea3c21cb6dd8
Dennis Kamau <dkamau476@gmail.com>
2022-10-25 02:25:10 -0700

    corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * printbeforemain - prints a sentence before
@@ -7,5 +8,6 @@
  */
 void printbeforemain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
