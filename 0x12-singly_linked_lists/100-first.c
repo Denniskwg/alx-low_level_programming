216ca6695d188198fc68b908c03c53739c0b5b70
Dennis Kamau <dkamau476@gmail.com>
2022-10-25 02:12:52 -0700

    updating header file, adding 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * printbeforemain - prints a sentence before
+ * the main function is executed
+ */
+void printbeforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,4 +19,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 size_t list_len(const list_t *h);
+void printbeforemain(void) __attribute__((constructor));
 #endif
