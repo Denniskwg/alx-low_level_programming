9c417cb1a73c749cb4de392d783d8f78c6561020
Dennis Kamau <dkamau476@gmail.com>
2022-10-31 07:06:30 -0700

    adding 1-print_binary.c and updated main.h

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print binary representation
+ */
+void print_binary(unsigned long int n)
+{
+	int i, flag = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 31; i >= 0; i--)
+	{
+		int mask = (1 << i);
+
+		if (n & mask)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else
+		{
+			if (flag > 0)
+				_putchar('0');
+		}
+	}
+}
