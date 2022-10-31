572626699ea9007e9ebd315322476ca849c0ed18
Dennis Kamau <dkamau476@gmail.com>
2022-10-31 12:27:01 -0700

    corrections

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int mask;
+	unsigned long int mask;
 	unsigned long int j;
 
-	if (n == NULL || index > 31 || index == 0)
+	if (n == NULL || index > 31)
 		return (-1);
 	mask = (1 << index);
 	j = *n;
