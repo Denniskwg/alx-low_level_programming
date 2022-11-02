9b16d0c4b47b697eacaaf89492beeb5ccf317fdb
Dennis Kamau <dkamau476@gmail.com>
2022-11-02 14:52:32 -0700

    correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND, 0600);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1 || errno == EACCES)
 		return (-1);
 	if (text_content == NULL && fd != -1)
