98669657409f9b814d513eeda8ca8c1b7c178c7b
Dennis Kamau <dkamau476@gmail.com>
2022-11-03 13:28:34 -0700

    correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_TRUNC | O_CREAT | O_RDWR, 0664);
+	fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 00664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
