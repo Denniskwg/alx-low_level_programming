3a214726792188f1c391f8460098084b7a46fd2e
Dennis Kamau <dkamau476@gmail.com>
2022-12-08 04:08:14 -0800

    correction for question 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,13 +11,18 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *current;
 	unsigned long i = 0;
 
+	if (!h)
+		return (i);
 	current = h->next;
-	current = current->prev;
-	while (current != NULL)
+	if (current)
 	{
-		printf("%d\n", current->n);
-		i++;
-		current = current->next;
+		current = current->prev;
+		while (current != NULL)
+		{
+			printf("%d\n", current->n);
+			i++;
+			current = current->next;
+		}
 	}
 	return (i);
 }
