949cdbd9799152e17e92e3593e0beaa1f934eb20
Dennis Kamau <dkamau476@gmail.com>
2022-12-09 12:56:17 -0800

    corrections for question 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,17 +14,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current, *new, *prev;
 	unsigned int i = 0;
 
-	if (*h == NULL && idx != 0)
-		return (NULL);
 	current = *h;
+	if (*h == NULL || idx == 0)
+	{
+		new = add_dnodeint(&current, n);
+		*h = new;
+		return (new);
+	}
 	while (current != NULL)
 	{
-		if (idx == 0)
-		{
-			new = add_dnodeint(&current, n);
-			*h = new;
-			return (new);
-		}
 		if (i == idx - 1)
 		{
 			prev = current->prev;
