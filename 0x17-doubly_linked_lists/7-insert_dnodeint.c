fd8f0ec2175f4956e1eecb555239e66e36cff931
Dennis Kamau <dkamau476@gmail.com>
2022-12-09 12:33:07 -0800

    corrections for question 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,19 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * insert_dnodeint_at_idx - insert a new node at given position
+ * @h: double pointer to head
+ * @idx: index to insert into
+ * @n: value to store in new node
+ * Return: Address of new node, or NULL if failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current, *new, *prev;
 	unsigned int i = 0;
 
-	if (*h == NULL)
+	if (*h == NULL && idx != 0)
 		return (NULL);
 	current = *h;
 	while (current != NULL)
 	{
-		prev = current->prev;
-		if (i == idx)
+		if (idx == 0)
 		{
+			new = add_dnodeint(&current, n);
+			*h = new;
+			return (new);
+		}
+		if (i == idx - 1)
+		{
+			prev = current->prev;
 			new = malloc(sizeof(dlistint_t));
 			if (new == NULL)
 				return (NULL);
