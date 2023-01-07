27922facbd5a20a79a79f1ee3c06d2aba8068ace
Dennis Kamau <dkamau476@gmail.com>
2023-01-07 05:53:14 -0800

    initial commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a hash table with members
+ * size and array
+ * @size: size is the lenght of hash table member array
+ * Return: pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return(NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+		return(NULL);
+	return(ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+# C - Hash tables
+  Excercise to learn hash table implementation in C
