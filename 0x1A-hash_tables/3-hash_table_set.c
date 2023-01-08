52dbe950341be420b14c39f66075bdd7f021e6b0
Dennis Kamau <dkamau476@gmail.com>
2023-01-08 02:35:50 -0800

    adding solution for question 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,12 +13,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index;
 	hash_node_t *item, *current;
+	int i = 0;
 
 	if (!key || ht == NULL || *key == '\0' || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			current->value = strdup(value);
+			i = 1;
+		}
+		current = current->next;
+	}
+	if (i == 1)
+		return (1);
 
+	current = ht->array[index];
 	item = malloc(sizeof(item));
 	if (item == NULL)
 		return (0);
