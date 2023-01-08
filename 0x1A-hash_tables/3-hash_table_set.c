0c1e381516549c05727243e4e3b7f44c13201f66
Dennis Kamau <dkamau476@gmail.com>
2023-01-08 01:22:14 -0800

    adding solution for question 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash table
+ * @key: key property of new item
+ * @value: value property of new item
+ * Return: 0 on failure 1 on success
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	size_t index;
+	hash_node_t *item, *current;
+
+	if (!key || ht == NULL || *key == '\0' || value == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	item = malloc(sizeof(item));
+	if (item == NULL)
+		return (0);
+	item->key = strdup(key);
+	item->value = strdup(value);
+	item->next = NULL;
+	if (current != NULL)
+	{
+		/*collision*/
+		item->next = current;
+	}
+	else
+		ht->array[index] = item;
+	return (1);
+}
