f6389d26d1b872b05b6230b91f624b5a5db27792
Dennis Kamau <dkamau476@gmail.com>
2023-01-07 22:46:25 -0800

    adding solution for question 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
@@ -18,5 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 		return(NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 	return(ht);
 }
