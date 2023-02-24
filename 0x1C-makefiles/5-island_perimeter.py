30fcf68ce451d298056403c5aae8d259c97448ab
Dennis Kamau <dkamau476@gmail.com>
2023-02-24 07:46:38 -0800

    edit solution for question 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,11 +15,11 @@ def island_perimeter(grid):
     length = len(grid)
     if length == 0:
         return 0
-    for l in grid:
-        for i in range(len(l)):
-            if l[i] == 1 and len(sides) == 0:
+    for line in grid:
+        for i in range(len(line)):
+            if line[i] == 1 and len(sides) == 0:
                 sides.append(3)
-            elif l[i] == 1 and len(sides) > 0:
+            elif line[i] == 1 and len(sides) > 0:
                 sides.append(2)
     sides.append(1)
     perimeter = 0
